75cb8cd59c8e3083ce068abcd9843c6889097a91
Heather Hudgeon <hhudgeon@gmail.com>
2026-02-25 20:20:08 -0600

    Initial scaffold working before restructure

---
+++ build/blocks-manifest.php
@@ -0,0 +1,25 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'hh-actor-profile' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'hh/hh-actor-profile',
+		'version' => '0.1.0',
+		'title' => 'Actor Profile Card',
+		'category' => 'design',
+		'icon' => 'id',
+		'description' => 'Display an actor profile with headshot, bio, and key details.',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'hh-actor-profile',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'viewScript' => 'file:./view.js'
+	)
+);
---
+++ build/hh-actor-profile/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '346416d3a4c0f5ff547f');
